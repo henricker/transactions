3406a7b952780da3420b48ed357219588f2db047
Henrique Vieira <henriquevieira@alu.ufc.br>
2022-01-25 17:31:47 -0300

    test: should be return an error if invalid credit card data is sended

---
+++ processor.service/usecase/process_transaction/process_transaction_test.go
@@ -0,0 +1,49 @@
+package process_transaction
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	entity "github.com/henricker/processor.service/domain/entities"
+	mock_repository "github.com/henricker/processor.service/domain/repository/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcessTransactionExecuteInvalidCreditCard(t *testing.T) {
+	input := TransactionDtoInput{
+		ID:                        "1",
+		AccountID:                 "1",
+		CreditCardNumber:          "4000000000000000000000000",
+		CreditCardExpirationMonth: 12,
+		CreditCardExpirationYear:  time.Now().Year(),
+		CreditCardName:            "Henrique Vieira",
+		CreditCardCVV:             123,
+		Amount:                    200,
+	}
+
+	expectedOutput := TransactionDtoOutput{
+		ID:           "1",
+		Status:       entity.REJECTED,
+		ErrorMessage: "Invalid credit card number",
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	/*To create mock using mockgen
+		"mockgen -destination=domain/repository/mock/transaction_mock.go -source=domain/repository/repository.go"
+	*/
+	repositoryMock := mock_repository.NewMockTransactionRepository(ctrl)
+
+	repositoryMock.EXPECT().
+		Insert(input.ID, input.AccountID, input.Amount, expectedOutput.Status, expectedOutput.ErrorMessage).
+		Return(nil)
+
+	usecase := NewProcessTransaction(repositoryMock)
+
+	output, err := usecase.Execute(input)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedOutput, output)
+}
