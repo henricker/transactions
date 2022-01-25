85612c905dad9164a092461c6c32bc22e6564fb4
Henrique Vieira <henriquevieira@alu.ufc.br>
2022-01-25 17:30:45 -0300

    refactor: exchange insert to Insert

--- processor.service/domain/repository/repository.go
+++ processor.service/domain/repository/repository.go
@@ -1,5 +1,5 @@
 package repository
 
 type TransactionRepository interface {
-	insert(id string, account string, ammount float64, status string, errorMessage string) error
+	Insert(id string, account string, ammount float64, status string, errorMessage string) error
 }
