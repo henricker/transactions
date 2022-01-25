729d898f98204faa6aca67889d190578d0e7af1d
Henrique Vieira <henriquevieira@alu.ufc.br>
2022-01-25 16:05:17 -0300

    feat: add transaction entity

---
+++ processor.service/domain/entities/transaction.go
@@ -0,0 +1,33 @@
+package entity
+
+import "errors"
+
+type Transaction struct {
+	ID           string
+	AccountID    string
+	Amount       float64
+	Status       string
+	ErrorMessage string
+	CreditCard   CreditCard
+}
+
+func NewTransaction() *Transaction {
+	return &Transaction{}
+}
+
+func (t *Transaction) IsValid() error {
+
+	if t.Amount > 1000 {
+		return errors.New("You dont have limit for this transaction")
+	}
+
+	if t.Amount <= 1 {
+		return errors.New("The amount must be greater than 1")
+	}
+
+	return nil
+}
+
+func (t *Transaction) SetCreditCard(card CreditCard) {
+	t.CreditCard = card
+}
