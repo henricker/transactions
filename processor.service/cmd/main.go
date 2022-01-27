7e5b806bcad47e07c582acc019cdd5a9873175ea
Henrique Vieira <henriquevieira@alu.ufc.br>
2022-01-26 22:15:40 -0300

    feat: add main function and building app

---
+++ processor.service/cmd/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/henricker/processor.service/adapter/broker/kafka"
+	"github.com/henricker/processor.service/adapter/presenter/transaction"
+	"github.com/henricker/processor.service/domain/factory"
+	"github.com/henricker/processor.service/usecase/process_transaction"
+)
+
+func main() {
+
+	//Create db
+	db, err := sql.Open("sqlite3", "test.db")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Create repository
+	repositoryFactory := factory.NewRepositoryDatabaseFactory(db)
+	repository := repositoryFactory.CreateTransactionRepository()
+
+	configMapProducer := &ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:9092",
+	}
+
+	//Create kafka utilities
+	kafkaPresenter := transaction.NewTransactionKafkaPresenter()
+
+	producer := kafka.NewKafkaProducer(configMapProducer, kafkaPresenter)
+
+	var msgChan = make(chan *ckafka.Message)
+
+	configMapConsumer := &ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:9092",
+		"client.id":         "goapp",
+		"group.id":          "goapp",
+	}
+
+	topics := []string{"transactions"}
+
+	consumer := kafka.NewConsumer(configMapConsumer, topics)
+
+	go consumer.Consume(msgChan) //Running on other thread
+
+	usecase := process_transaction.NewProcessTransaction(repository, producer, "transactions_result")
+
+	for msg := range msgChan {
+		var input process_transaction.TransactionDtoInput
+		json.Unmarshal(msg.Value, &input)
+		usecase.Execute(input)
+	}
+
+}
