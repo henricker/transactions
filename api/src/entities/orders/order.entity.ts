5e4387c2406e5abf9cabd3fb1f0688be0bde78ed
Henrique Vieira <henriquevieira@alu.ufc.br>
2022-02-07 10:10:44 -0300

    refactor: add new fields and and new type rejected to OrderStatus

--- api/src/entities/orders/order.entity.ts
+++ api/src/entities/orders/order.entity.ts
@@ -10,8 +10,8 @@ import { Account } from '../accounts/account.entity';
 export enum OrderStatus {
   Pending = 'pending',
   Approved = 'approved',
+  Rejected = 'rejected'
 }
-
 @Entity({ name: 'orders' })
 export class Order {
   @PrimaryGeneratedColumn()
@@ -29,8 +29,14 @@ export class Order {
   @Column({ nullable: false })
   credit_card_name: string;
 
-  @Column({ nullable: false })
+  @Column({ nullable: false, name: 'cvv' })
   credit_card_cvv: number;
+
+  @Column({ nullable: false })
+  credit_card_expiration_month: number
+
+  @Column({ nullable: true })
+  credit_card_expiration_year: number;
 
   @Column({ nullable: false, default: OrderStatus.Pending })
   status: OrderStatus;
